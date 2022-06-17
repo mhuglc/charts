cd815f76fe5ac6c70c0e8c108db06162bac364e7
aggrimm <agrimmps@gmail.com>
2022-06-17 10:59:13 -0400

    Update statepop2.js

--- Drinking_Water/basin_pop/statepop2.js
+++ Drinking_Water/basin_pop/statepop2.js
@@ -1,121 +1,5 @@
 statesdata=[
- {
-   "name": "Alabama",
-   "PopDensity": "95.4",
-   "Population": 4874747,
-   "rCA": 0.388,
-   "rNJ": 0.079,
-   "rCAx": 0.623,
-   "rNJx": 0.281
- },
- {
-   "name": "Alaska",
-   "PopDensity": "1.3",
-   "Population": 739795,
-   "rCA": 0.005,
-   "rNJ": 0.001,
-   "rCAx": 0.073,
-   "rNJx": 0.033
- },
- {
-   "name": "Arizona",
-   "PopDensity": "58.3",
-   "Population": 7016270,
-   "rCA": 0.237,
-   "rNJ": 0.048,
-   "rCAx": 0.487,
-   "rNJx": 0.219
- },
- {
-   "name": "Arkansas",
-   "PopDensity": "56.9",
-   "Population": 3004279,
-   "rCA": 0.231,
-   "rNJ": 0.047,
-   "rCAx": 0.481,
-   "rNJx": 0.217
- },
- {
-   "name": "California",
-   "PopDensity": "246.1",
-   "Population": 39536653,
-   "rCA": 1,
-   "rNJ": 0.203,
-   "rCAx": 1,
-   "rNJx": 0.451
- },
- {
-   "name": "Colorado",
-   "PopDensity": "50.8",
-   "Population": 5607154,
-   "rCA": 0.206,
-   "rNJ": 0.042,
-   "rCAx": 0.454,
-   "rNJx": 0.205
- },
- {
-   "name": "Connecticut",
-   "PopDensity": "742.6",
-   "Population": 3588184,
-   "rCA": 3.017,
-   "rNJ": 0.614,
-   "rCAx": 1.737,
-   "rNJx": 0.783
- },
- {
-   "name": "Delaware",
-   "PopDensity": "475.1",
-   "Population": 961939,
-   "rCA": 1.931,
-   "rNJ": 0.393,
-   "rCAx": 1.389,
-   "rNJx": 0.627
- },
- {
-   "name": "District of Columbia",
-   "PopDensity": "10,588.8",
-   "Population": 693972,
-   "rCA": 43.026,
-   "rNJ": 8.75,
-   "rCAx": 6.559,
-   "rNJx": 2.958
- },
- {
-   "name": "Florida",
-   "PopDensity": "364.6",
-   "Population": 20984400,
-   "rCA": 1.482,
-   "rNJ": 0.301,
-   "rCAx": 1.217,
-   "rNJx": 0.549
- },
- {
-   "name": "Georgia",
-   "PopDensity": "173.7",
-   "Population": 10429379,
-   "rCA": 0.706,
-   "rNJ": 0.144,
-   "rCAx": 0.84,
-   "rNJx": 0.379
- },
- {
-   "name": "Hawaii",
-   "PopDensity": "218.6",
-   "Population": 1427538,
-   "rCA": 0.888,
-   "rNJ": 0.181,
-   "rCAx": 0.942,
-   "rNJx": 0.425
- },
- {
-   "name": "Idaho",
-   "PopDensity": "19.5",
-   "Population": 1716943,
-   "rCA": 0.079,
-   "rNJ": 0.016,
-   "rCAx": 0.281,
-   "rNJx": 0.127
- },
+
  {
    "name": "Illinois",
    "PopDensity": "232",
@@ -133,69 +17,6 @@ statesdata=[
    "rNJ": 0.152,
    "rCAx": 0.863,
    "rNJx": 0.389
- },
- {
-   "name": "Iowa",
-   "PopDensity": "55.3",
-   "Population": 3145711,
-   "rCA": 0.225,
-   "rNJ": 0.046,
-   "rCAx": 0.474,
-   "rNJx": 0.214
- },
- {
-   "name": "Kansas",
-   "PopDensity": "35.4",
-   "Population": 2913123,
-   "rCA": 0.144,
-   "rNJ": 0.029,
-   "rCAx": 0.379,
-   "rNJx": 0.171
- },
- {
-   "name": "Kentucky",
-   "PopDensity": "111.3",
-   "Population": 4454189,
-   "rCA": 0.452,
-   "rNJ": 0.092,
-   "rCAx": 0.672,
-   "rNJx": 0.303
- },
- {
-   "name": "Louisiana",
-   "PopDensity": "107.1",
-   "Population": 4684333,
-   "rCA": 0.435,
-   "rNJ": 0.089,
-   "rCAx": 0.66,
-   "rNJx": 0.297
- },
- {
-   "name": "Maine",
-   "PopDensity": "43.1",
-   "Population": 1335907,
-   "rCA": 0.175,
-   "rNJ": 0.036,
-   "rCAx": 0.418,
-   "rNJx": 0.189
- },
- {
-   "name": "Maryland",
-   "PopDensity": "610.8",
-   "Population": 6052177,
-   "rCA": 2.482,
-   "rNJ": 0.505,
-   "rCAx": 1.575,
-   "rNJx": 0.71
- },
- {
-   "name": "Massachusetts",
-   "PopDensity": "858",
-   "Population": 6859819,
-   "rCA": 3.486,
-   "rNJ": 0.709,
-   "rCAx": 1.867,
-   "rNJx": 0.842
  },
  {
    "name": "Michigan",
@@ -216,78 +37,6 @@ statesdata=[
    "rNJx": 0.237
  },
  {
-   "name": "Mississippi",
-   "PopDensity": "63.7",
-   "Population": 2984100,
-   "rCA": 0.259,
-   "rNJ": 0.053,
-   "rCAx": 0.509,
-   "rNJx": 0.229
- },
- {
-   "name": "Missouri",
-   "PopDensity": "87.9",
-   "Population": 6113532,
-   "rCA": 0.357,
-   "rNJ": 0.073,
-   "rCAx": 0.598,
-   "rNJx": 0.27
- },
- {
-   "name": "Montana",
-   "PopDensity": "7",
-   "Population": 1050493,
-   "rCA": 0.028,
-   "rNJ": 0.006,
-   "rCAx": 0.169,
-   "rNJx": 0.076
- },
- {
-   "name": "Nebraska",
-   "PopDensity": "24.3",
-   "Population": 1920076,
-   "rCA": 0.099,
-   "rNJ": 0.02,
-   "rCAx": 0.314,
-   "rNJx": 0.142
- },
- {
-   "name": "Nevada",
-   "PopDensity": "25.4",
-   "Population": 2998039,
-   "rCA": 0.103,
-   "rNJ": 0.021,
-   "rCAx": 0.321,
-   "rNJx": 0.145
- },
- {
-   "name": "New Hampshire",
-   "PopDensity": "147.8",
-   "Population": 1342795,
-   "rCA": 0.601,
-   "rNJ": 0.122,
-   "rCAx": 0.775,
-   "rNJx": 0.349
- },
- {
-   "name": "New Jersey",
-   "PopDensity": "1,210.10",
-   "Population": 9005644,
-   "rCA": 4.917,
-   "rNJ": 1,
-   "rCAx": 2.217,
-   "rNJx": 1
- },
- {
-   "name": "New Mexico",
-   "PopDensity": "17.2",
-   "Population": 2088070,
-   "rCA": 0.07,
-   "rNJ": 0.014,
-   "rCAx": 0.264,
-   "rNJx": 0.119
- },
- {
    "name": "New York",
    "PopDensity": "417",
    "Population": 19849399,
@@ -295,24 +44,6 @@ statesdata=[
    "rNJ": 0.345,
    "rCAx": 1.302,
    "rNJx": 0.587
- },
- {
-   "name": "North Carolina",
-   "PopDensity": "202.6",
-   "Population": 10273419,
-   "rCA": 0.823,
-   "rNJ": 0.167,
-   "rCAx": 0.907,
-   "rNJx": 0.409
- },
- {
-   "name": "North Dakota",
-   "PopDensity": "10.5",
-   "Population": 755393,
-   "rCA": 0.043,
-   "rNJ": 0.009,
-   "rCAx": 0.207,
-   "rNJx": 0.093
  },
  {
    "name": "Ohio",
@@ -324,24 +55,6 @@ statesdata=[
    "rNJx": 0.484
  },
  {
-   "name": "Oklahoma",
-   "PopDensity": "56.1",
-   "Population": 3930864,
-   "rCA": 0.228,
-   "rNJ": 0.046,
-   "rCAx": 0.477,
-   "rNJx": 0.215
- },
- {
-   "name": "Oregon",
-   "PopDensity": "40.9",
-   "Population": 4142776,
-   "rCA": 0.166,
-   "rNJ": 0.034,
-   "rCAx": 0.408,
-   "rNJx": 0.184
- },
- {
    "name": "Pennsylvania",
    "PopDensity": "285.5",
    "Population": 12805537,
@@ -349,96 +62,6 @@ statesdata=[
    "rNJ": 0.236,
    "rCAx": 1.077,
    "rNJx": 0.486
- },
- {
-   "name": "Rhode Island",
-   "PopDensity": "1,017.10",
-   "Population": 1059639,
-   "rCA": 4.133,
-   "rNJ": 0.841,
-   "rCAx": 2.033,
-   "rNJx": 0.917
- },
- {
-   "name": "South Carolina",
-   "PopDensity": "158.8",
-   "Population": 5024369,
-   "rCA": 0.645,
-   "rNJ": 0.131,
-   "rCAx": 0.803,
-   "rNJx": 0.362
- },
- {
-   "name": "South Dakota",
-   "PopDensity": "11.1",
-   "Population": 869666,
-   "rCA": 0.045,
-   "rNJ": 0.009,
-   "rCAx": 0.212,
-   "rNJx": 0.096
- },
- {
-   "name": "Tennessee",
-   "PopDensity": "157.5",
-   "Population": 6715984,
-   "rCA": 0.64,
-   "rNJ": 0.13,
-   "rCAx": 0.8,
-   "rNJx": 0.361
- },
- {
-   "name": "Texas",
-   "PopDensity": "101.2",
-   "Population": 28304596,
-   "rCA": 0.411,
-   "rNJ": 0.084,
-   "rCAx": 0.641,
-   "rNJx": 0.289
- },
- {
-   "name": "Utah",
-   "PopDensity": "35.3",
-   "Population": 3101833,
-   "rCA": 0.143,
-   "rNJ": 0.029,
-   "rCAx": 0.379,
-   "rNJx": 0.171
- },
- {
-   "name": "Vermont",
-   "PopDensity": "68",
-   "Population": 623657,
-   "rCA": 0.276,
-   "rNJ": 0.056,
-   "rCAx": 0.526,
-   "rNJx": 0.237
- },
- {
-   "name": "Virginia",
-   "PopDensity": "209.2",
-   "Population": 8470020,
-   "rCA": 0.85,
-   "rNJ": 0.173,
-   "rCAx": 0.922,
-   "rNJx": 0.416
- },
- {
-   "name": "Washington",
-   "PopDensity": "104.9",
-   "Population": 7405743,
-   "rCA": 0.426,
-   "rNJ": 0.087,
-   "rCAx": 0.653,
-   "rNJx": 0.294
- },
- {
-   "name": "West Virginia",
-   "PopDensity": "77.1",
-   "Population": 1815857,
-   "rCA": 0.313,
-   "rNJ": 0.064,
-   "rCAx": 0.56,
-   "rNJx": 0.252
  },
  {
    "name": "Wisconsin",
@@ -449,13 +72,4 @@ statesdata=[
    "rCAx": 0.656,
    "rNJx": 0.296
  },
- {
-   "name": "Wyoming",
-   "PopDensity": "6",
-   "Population": 579315,
-   "rCA": 0.024,
-   "rNJ": 0.005,
-   "rCAx": 0.156,
-   "rNJx": 0.07
- }
 ];
