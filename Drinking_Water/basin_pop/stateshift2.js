7506aad9bf42f061898b800f641b41cc84e0ad31
aggrimm <agrimmps@gmail.com>
2022-06-17 10:15:23 -0400

    Create stateshift2.js

---
+++ Drinking_Water/basin_pop/stateshift2.js
@@ -0,0 +1,206 @@
+stateoffset=[
+ {
+   "name": "Alabama",
+   "offset": [0,0]
+ },
+ {
+   "name": "Alaska",
+   "offset": [0,0]
+ },
+ {
+   "name": "Arizona",
+   "offset": [0,0]
+ },
+ {
+   "name": "Arkansas",
+   "offset": [0,0]
+ },
+ {
+   "name": "California",
+   "offset": [0,0]
+ },
+ {
+   "name": "Colorado",
+   "offset": [0,0]
+ },
+ {
+   "name": "Connecticut",
+   "offset": [4.2,3.6]
+ },
+ {
+   "name": "Delaware",
+   "offset": [2.4,0]
+ },
+ {
+   "name": "District of Columbia",
+   "offset": [0,0]
+ },
+ {
+   "name": "Florida",
+   "offset": [0,-0.5]
+ },
+ {
+   "name": "Georgia",
+   "offset": [0,0]
+ },
+ {
+   "name": "Hawaii",
+   "offset": [0,0]
+ },
+ {
+   "name": "Idaho",
+   "offset": [0,-1]
+ },
+ {
+   "name": "Illinois",
+   "offset": [0,0]
+ },
+ {
+   "name": "Indiana",
+   "offset": [0,0]
+ },
+ {
+   "name": "Iowa",
+   "offset": [0,0]
+ },
+ {
+   "name": "Kansas",
+   "offset": [0,0]
+ },
+ {
+   "name": "Kentucky",
+   "offset": [0,0]
+ },
+ {
+   "name": "Louisiana",
+   "offset": [0,0]
+ },
+ {
+   "name": "Maine",
+   "offset": [2.4,3.5]
+ },
+ {
+   "name": "Maryland",
+   "offset": [1.2,-.4]
+ },
+ {
+   "name": "Massachusetts",
+   "offset": [5.3,4.2]
+ },
+ {
+   "name": "Michigan",
+   "offset": [0,0]
+ },
+ {
+   "name": "Minnesota",
+   "offset": [-0.8,0]
+ },
+ {
+   "name": "Mississippi",
+   "offset": [0,0]
+ },
+ {
+   "name": "Missouri",
+   "offset": [0,0]
+ },
+ {
+   "name": "Montana",
+   "offset": [0,0]
+ },
+ {
+   "name": "Nebraska",
+   "offset": [0,0]
+ },
+ {
+   "name": "Nevada",
+   "offset": [0,0]
+ },
+ {
+   "name": "New Hampshire",
+   "offset": [3.3,4.5]
+ },
+ {
+   "name": "New Jersey",
+   "offset": [2.2,2.2]
+ },
+ {
+   "name": "New Mexico",
+   "offset": [0,0]
+ },
+ {
+   "name": "New York",
+   "offset": [2.7,3.8]
+ },
+ {
+   "name": "North Carolina",
+   "offset": [0,0]
+ },
+ {
+   "name": "North Dakota",
+   "offset": [0,0]
+ },
+ {
+   "name": "Ohio",
+   "offset": [0,0]
+ },
+ {
+   "name": "Oklahoma",
+   "offset": [1,0]
+ },
+ {
+   "name": "Oregon",
+   "offset": [0,0]
+ },
+ {
+   "name": "Pennsylvania",
+   "offset": [0.5,0.3]
+ },
+ {
+   "name": "Rhode Island",
+   "offset": [5.4,3.6]
+ },
+ {
+   "name": "South Carolina",
+   "offset": [0,0]
+ },
+ {
+   "name": "South Dakota",
+   "offset": [0,0]
+ },
+ {
+   "name": "Tennessee",
+   "offset": [0,0]
+ },
+ {
+   "name": "Texas",
+   "offset": [0,0]
+ },
+ {
+   "name": "Utah",
+   "offset": [0,0]
+ },
+ {
+   "name": "Vermont",
+   "offset": [3.3,4.3]
+ },
+ {
+   "name": "Virginia",
+   "offset": [-0.3,0]
+ },
+ {
+   "name": "Washington",
+   "offset": [0,0]
+ },
+ {
+   "name": "West Virginia",
+   "offset": [-.5,-.3]
+ },
+ {
+   "name": "Wisconsin",
+   "offset": [0,0]
+ },
+ {
+   "name": "Wyoming",
+   "offset": [0,0]
+ }
+];
