d6c263a77a69389fa922989b80d857e99db8b985
aggrimm <agrimmps@gmail.com>
2022-07-04 20:16:35 -0400

    Update statepop2.js

--- Drinking_Water/basin_pop/statepop2.js
+++ Drinking_Water/basin_pop/statepop2.js
@@ -6,8 +6,10 @@ statesdata=[
    "Population": 6523589,
    "rCA": 0.943,
    "rNJ": 0.192,
+   "rMI": 1.101,
    "rCAx": 0.971,
-   "rNJx": 0.438
+   "rNJx": 0.438,
+   "rMIx": 1.049
  },
  {
    "name": "Indiana",
@@ -15,8 +17,10 @@ statesdata=[
    "Population": 1370026,
    "rCA": 0.745,
    "rNJ": 0.152,
+   "rMI": 0.3676,
    "rCAx": 0.863,
-   "rNJx": 0.389
+   "rNJx": 0.389,
+   "rMIx": 0.6063
  },
  {
    "name": "Michigan",
@@ -24,8 +28,10 @@ statesdata=[
    "Population": 9897200,
    "rCA": 0.711,
    "rNJ": 0.145,
+   "rMI": 1,
    "rCAx": 0.843,
-   "rNJx": 0.38
+   "rNJx": 0.38,
+   "rMIx": 1
  },
  {
    "name": "Minnesota",
@@ -33,8 +39,10 @@ statesdata=[
    "Population": 120547,
    "rCA": 0.277,
    "rNJ": 0.056,
+   "rMI": 0.0135,
    "rCAx": 0.526,
-   "rNJx": 0.237
+   "rNJx": 0.237,
+   "rMIx": 0.116
  },
  {
    "name": "New York",
@@ -42,8 +50,10 @@ statesdata=[
    "Population": 4180721,
    "rCA": 1.694,
    "rNJ": 0.345,
+   "rMI": 0.7488,
    "rCAx": 1.302,
-   "rNJx": 0.587
+   "rNJx": 0.587,
+   "rMIx": 0.865
  },
  {
    "name": "Ohio",
@@ -51,8 +61,10 @@ statesdata=[
    "Population": 4419481,
    "rCA": 1.151,
    "rNJ": 0.234,
+   "rMI": 0.963,
    "rCAx": 1.073,
-   "rNJx": 0.484
+   "rNJx": 0.484,
+   "rMIx": 0.982
  },
  {
    "name": "Pennsylvania",
@@ -60,8 +72,10 @@ statesdata=[
    "Population": 294809,
    "rCA": 1.16,
    "rNJ": 0.236,
+   "rMI": 0.063,
    "rCAx": 1.077,
-   "rNJx": 0.486
+   "rNJx": 0.486,
+   "rMIx": 0.250
  },
  {
    "name": "Wisconsin",
@@ -69,7 +83,9 @@ statesdata=[
    "Population": 2753981,
    "rCA": 0.431,
    "rNJ": 0.088,
+   "rMI": 0.411,
    "rCAx": 0.656,
-   "rNJx": 0.296
+   "rNJx": 0.296,
+   "rMIx": 0.641
  },
 ];
