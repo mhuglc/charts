d07a8baa142e2a9a6c9683ab2a9c2346fc6a89b8
aggrimm <agrimmps@gmail.com>
2022-07-04 20:29:20 -0400

    Update statepop2.js

--- Drinking_Water/basin_pop/statepop2.js
+++ Drinking_Water/basin_pop/statepop2.js
@@ -4,88 +4,56 @@ statesdata=[
    "name": "Illinois",
    "PopDensity": "232",
    "Population": 6523589,
-   "rCA": 0.943,
-   "rNJ": 0.192,
-   "rMI": 1.101,
-   "rCAx": 0.971,
-   "rNJx": 0.438,
-   "rMIx": 1.049
+   "rCA": 1.101,
+   "rCAx": 1.049
  },
  {
    "name": "Indiana",
    "PopDensity": "183.4",
    "Population": 1370026,
-   "rCA": 0.745,
-   "rNJ": 0.152,
-   "rMI": 0.3676,
-   "rCAx": 0.863,
-   "rNJx": 0.389,
-   "rMIx": 0.6063
+   "rCA": 0.3676,
+   "rCAx": 0.6063
  },
  {
    "name": "Michigan",
    "PopDensity": "175",
    "Population": 9897200,
-   "rCA": 0.711,
-   "rNJ": 0.145,
-   "rMI": 1,
-   "rCAx": 0.843,
-   "rNJx": 0.38,
-   "rMIx": 1
+   "rCA": 1,
+   "rCAx": 1
  },
  {
    "name": "Minnesota",
    "PopDensity": "68.1",
    "Population": 120547,
-   "rCA": 0.277,
-   "rNJ": 0.056,
-   "rMI": 0.0135,
-   "rCAx": 0.526,
-   "rNJx": 0.237,
-   "rMIx": 0.116
+   "rCA": 0.0135,
+   "rCAx": 0.116
  },
  {
    "name": "New York",
    "PopDensity": "417",
    "Population": 4180721,
-   "rCA": 1.694,
-   "rNJ": 0.345,
-   "rMI": 0.7488,
-   "rCAx": 1.302,
-   "rNJx": 0.587,
-   "rMIx": 0.865
+   "rCA": 0.7488,
+   "rCAx": 0.865
  },
  {
    "name": "Ohio",
    "PopDensity": "283.2",
    "Population": 4419481,
-   "rCA": 1.151,
-   "rNJ": 0.234,
-   "rMI": 0.963,
-   "rCAx": 1.073,
-   "rNJx": 0.484,
-   "rMIx": 0.982
+   "rCA": 0.963,
+   "rCAx": 0.982
  },
  {
    "name": "Pennsylvania",
    "PopDensity": "285.5",
    "Population": 294809,
-   "rCA": 1.16,
-   "rNJ": 0.236,
-   "rMI": 0.063,
-   "rCAx": 1.077,
-   "rNJx": 0.486,
-   "rMIx": 0.250
+   "rCA": 0.063,
+   "rCAx": 0.250
  },
  {
    "name": "Wisconsin",
    "PopDensity": "106",
    "Population": 2753981,
-   "rCA": 0.431,
-   "rNJ": 0.088,
-   "rMI": 0.411,
-   "rCAx": 0.656,
-   "rNJx": 0.296,
-   "rMIx": 0.641
+   "rCA": 0.411,
+   "rCAx": 0.641
  },
 ];
